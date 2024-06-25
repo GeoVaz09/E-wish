08967b06feae346c741d05722d3a3b9d2e694855
GeoVaz09 <vazakasgeorge@gmail.com>
2024-06-25 18:10:18 +0300

    big ui improvements

--- E-wish website/E-wish website.py
+++ E-wish website/E-wish website.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
+from distutils.log import debug 
+from fileinput import filename 
+from flask import *  
+app = Flask(__name__)   
+  
+@app.route('/')   
+def main():   
+    return render_template("index.html")   
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-     return render_template("index.html")
-
-
-@app.route("/<wish>")
-def wish_type(wish):
-    return render_template("wish.html", wish=wish)
-
+@app.route("/wish/<wish>/<image>/<sender>/")
+def wish_type(wish, image, sender):
+    return render_template("wish.html", wish=wish, image=image, sender=sender)
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', debug=True, port=80)
