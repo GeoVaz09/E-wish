d19f90587583600e52d3683421f8d46019b730ae
GeoVaz09 <vazakasgeorge@gmail.com>
2024-06-29 17:01:23 +0300

    part 7

--- E-wish website/E-wish website.py
+++ E-wish website/E-wish website.py
@@ -1,24 +1,24 @@
-from flask import Flask, render_template, request, jsonify
-from distutils.log import debug 
-from fileinput import filename 
-from flask import *
+from flask import Flask, request, render_template, jsonify
 import random
 import string
 import os
 
-app = Flask(__name__)   
+app = Flask(__name__)
 
 STORAGE_FILE = 'url_codes.txt'
 
 def generate_code(length=6):
+    """Generate a random string of fixed length."""
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 
 def save_url_code(url, code):
+    """Save the URL and its corresponding code to a file."""
     with open(STORAGE_FILE, 'a') as f:
         f.write(f"{code},{url}\n")
 
 def get_url_by_code(code):
+    """Retrieve the URL by its code from the file."""
     if not os.path.exists(STORAGE_FILE):
         return None
     with open(STORAGE_FILE, 'r') as f:
@@ -28,15 +28,12 @@ def get_url_by_code(code):
             if saved_code == code:
                 return saved_url
     return None
-  
-@app.route('/')   
-def main():   
-    return render_template("index.html")   
 
-@app.route("/wish/<sender>/<wish>/<image>/")
-def wish_type(wish, image, sender):
-    return render_template("wish.html", sender=sender, wish=wish, image=image)
+@app.route('/')
+def index():
+     return render_template('index.html')
 
+# POST request to save URL and get a code
 @app.route('/post_url', methods=['POST'])
 def post_url():
     data = request.get_json()
@@ -47,13 +44,13 @@ def post_url():
     save_url_code(url, code)
     return jsonify({"message": "URL saved", "code": code})
 
-@app.route('/get_url/<code>', methods=['GET'])
-def get_url(code):
+# GET request to retrieve URL by code
+@app.route('/wish/<sender>/<wish>/<code>', methods=['GET'])
+def get_wish(sender, wish, code):
     url = get_url_by_code(code)
-    if url:
-        return jsonify({"url": url})
-    else:
-        return jsonify({"error": "Code not found"}), 404
+    if url is None:
+        url = ""
+    return render_template('wish.html', sender=sender, wish=wish, image=url)
 
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', debug=True, port=80)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
