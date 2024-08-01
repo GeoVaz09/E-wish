f8d70b77ad58763960ed1ddb829e8abc67116605
GeoVaz09 <vazakasgeorge@gmail.com>
2024-08-01 19:00:23 +0300

    Update E-wish website.py

--- E-wish website/E-wish website.py
+++ E-wish website/E-wish website.py
@@ -52,4 +52,4 @@ def get_wish(sender, wish, code):
     return render_template('wish.html', sender=sender, wish=wish, image=url)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=4768)
